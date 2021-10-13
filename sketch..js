cc4fc65f1a81fe742f50096d5e5ff41ef222149a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-13 12:08:02 -0500

    Create sketch..js

---
+++ sketch..js
@@ -0,0 +1,18 @@
+let dieOne;
+let dieTwo;
+let sum;
+function setup() {
+  createCanvas(400, 400);
+  dieOne = Math.floor(Math.random()*6 +1 );
+  dieTwo = Math.floor(Math.random()*6 +1 );
+  sum = dieOne + dieTwo;
+}
+
+function draw() {
+  background(220);
+  textSize(50);
+  text(dieOne, 50,50)
+  text(dieTwo, 100,50)
+  text("The Sum is: " + sum, 50,100)
+  
+}
