2326a640e2edcca8ce0e607b4b705946748e731d
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-28 09:56:33 -0500

    Update sketch..js

--- sketch..js
+++ sketch..js
@@ -7,35 +7,42 @@ let money;
 
 function setup() {
   createCanvas(400, 400);
-  money  =50;
+  loadFont('spaceFont.ttf');
+  money = 50;
   design();
-  
-  
-  target = createInput();
-  target.position(10,220);
-  risk = createInput();
-  risk.position(10,320);
 }
 
 function draw() {
-  background(37,150,190);
+  background(37, 150, 190);
   design();
-  if(keyIsDown(13)){
-    dieOne = Math.floor(Math.random()*6 +1 );
-    dieTwo = Math.floor(Math.random()*6 +1 );
-  }else{
+  if (keyIsDown(13)) {
+    dieOne = Math.floor(Math.random() * 6 + 1);
+    dieTwo = Math.floor(Math.random() * 6 + 1);
+  } else {
     sum = dieOne + dieTwo;
   }
 }
-function design(){
-  fill(226,135,67);
-  text("Dice Game", 150,20);
-  text("Money: " + money,10, 50);
-  text("Die 1: " + dieOne, 10,100);
-  textSize(24);
-  text("What Number are you trying to roll?",10,200)
-  text("How much do you wanna risk?",10,300)
-  text("The Sum is: " + sum, 50,100) ;
-  
+function design() {
+  loadFont('spaceFont.ttf');
+  stroke(226,135,67);
+  strokeWeight(1);
+  fill(226, 135, 67);
+  text("Dice Game", 150, 20);
+  text("Money: " + money, 10, 50);
+  text("Die 1 ", 10, 100);
+  text("Die 2 ", 75, 100);
+  fill(238, 238, 228);
+  rect(10, 110, 55, 55, 20);
+  rect(75, 110, 55, 55, 20);
+  textSize(17);
+  fill(226, 135, 67);
+  text("What Number are you trying to roll?", 10, 200);
+  text("How much do you wanna risk?", 10, 300);
+  target = createInput();
+  target.position(10, 220);
+  target.size(30);
+  risk = createInput();
+  risk.position(10, 320);
+  risk.size(30);
+  //text("The Sum is: " + sum, 50,100) ;
 }
-
