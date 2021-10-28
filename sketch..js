c17faec9edac4b507e4dcd09dbd15908ab4977a2
corbin Boughen <50217007+theBough@users.noreply.github.com>
2021-10-28 09:25:34 -0500

    Update sketch..js

--- sketch..js
+++ sketch..js
@@ -3,12 +3,14 @@ let dieTwo;
 let sum;
 let target;
 let risk;
+let money;
 
 function setup() {
   createCanvas(400, 400);
-  dieOne = Math.floor(Math.random()*6 +1 );
-  dieTwo = Math.floor(Math.random()*6 +1 );
-  sum = dieOne + dieTwo;
+  money  =50;
+  design();
+  
+  
   target = createInput();
   target.position(10,220);
   risk = createInput();
@@ -16,19 +18,24 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  textSize(24);
-  text("What Number are you tring to roll?",10,200)
-  text("How much do you wanna risk?",10,300)
-  text(dieOne, 50,50)
-  text(dieTwo, 100,50)
+  background(37,150,190);
+  design();
   if(keyIsDown(13)){
     dieOne = Math.floor(Math.random()*6 +1 );
     dieTwo = Math.floor(Math.random()*6 +1 );
   }else{
     sum = dieOne + dieTwo;
   }
-    
+}
+function design(){
+  fill(226,135,67);
+  text("Dice Game", 150,20);
+  text("Money: " + money,10, 50);
+  text("Die 1: " + dieOne, 10,100);
+  textSize(24);
+  text("What Number are you trying to roll?",10,200)
+  text("How much do you wanna risk?",10,300)
   text("The Sum is: " + sum, 50,100) ;
+  
 }
 
